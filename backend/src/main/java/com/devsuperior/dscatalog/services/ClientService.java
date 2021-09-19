028b682d70e773eb003fa490b9693db74da97e8b
Fabricio Araujo <fabriciofa@gmail.com>
2021-09-18 23:53:08 -0300

    Find all clients paged

--- backend/src/main/java/com/devsuperior/dscatalog/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ClientService.java
@@ -1,8 +1,13 @@
 package com.devsuperior.dscatalog.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.ClientDTO;
+import com.devsuperior.dscatalog.entities.Client;
 import com.devsuperior.dscatalog.repositories.ClientRepository;
 
 @Service
@@ -10,5 +15,11 @@ public class ClientService {
 
 	@Autowired
 	private ClientRepository clientRepository;
-	
+
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = clientRepository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
+	}
+
 }
