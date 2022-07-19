302a0c1d96baafaae4c644f567fa9f6e0c6bf9cf
Fabricio Araújo <fabriciofa@gmail.com>
2022-07-19 09:53:22 -0300

    Product crud form HTML

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,7 +1,29 @@
+import './styles.css';
+
 const Form = () => {
-    return(
-        <h1>Form</h1>
-    )
-}
+  return (
+    <div className="product-crud-container">
+      <div className="base-card product-crud-form-card">
+        <h1 className="product-curd-form-title">DADOS DO PRODUTO</h1>
+        <form>
+          <div className="row">
+            <div className="col-lg-6">
+                <input type="text" className="form-control base-input" />
+                <input type="text" className="form-control base-input" />
+                <input type="text" className="form-control base-input" />
+            </div>
+            <div className="col-lg-6">
+                <textarea name="" rows={10} className="form-control base-input" ></textarea>
+            </div>
+          </div>
+          <div>
+            <button className="btn btn-outline-danger">CANCELAR</button>
+            <button className="btn btn-primary text-white">SALVAR</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Form;
--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -26,7 +26,7 @@ const List = () => {
   }, []);
 
   return (
-    <>
+    <div className="product-crud-container">
       <div className="product-crud-bar-container">
         <Link to="/admin/products/create">
           <button className="btn btn-primary text-white btn-crud-add">
@@ -40,7 +40,7 @@ const List = () => {
           product => <ProductCrudCard product={product} />
         )}
       </div>
-    </>
+    </div>
   );
 };
 
