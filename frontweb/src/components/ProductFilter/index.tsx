ce56fafbbba38fa353290e45c4a5a24debf2da6b
Fabricio Araújo <fabriciofa@gmail.com>
2022-07-26 10:42:35 -0300

    ProductFilter input name layout

---
+++ frontweb/src/components/ProductFilter/index.tsx
@@ -0,0 +1,29 @@
+import './styles.css';
+import { ReactComponent as SearchIcon } from 'assets/images/search-icon.svg';
+
+const ProductFilter = () => {
+  return (
+    <div className="base-card product-filter-container">
+      <form className="product-filter-form">
+        <div className="product-filter-name-container">
+          <input
+            type="text"
+            placeholder="Nome do produto"
+            className="form-control"
+          />
+          <SearchIcon />
+        </div>
+        <div className="product-filter-bottom-container">
+          <div className="product-filter-category-container">
+            <select name="" id="">
+              <option value="">Livros</option>
+            </select>
+          </div>
+          <button className="btn btn-outline-secondary">LIMPAR</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ProductFilter;
